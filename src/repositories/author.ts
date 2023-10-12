f0d6d4ea307e68f9b4d164781e8094655384a912
belovetech <belovetech@gmail.com>
2023-10-12 21:08:48 +0100

    (feat): create author and post repositories

---
+++ src/repositories/author.ts
@@ -0,0 +1,5 @@
+import { dataSource } from '@medusajs/medusa/dist/loaders/database';
+import { Author } from '../models/author';
+
+const AuthorRepository = dataSource.getRepository(Author);
+export default AuthorRepository;
